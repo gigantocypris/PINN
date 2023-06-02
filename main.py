fixed the negative sign in f, added documentation

main.py
@@ -4,31 +4,31 @@ from utils import create_data, NeuralNetwork, get_pde_loss, train, test, get_k0,
 import matplotlib.pyplot as plt
 import numpy as np
 
-
-
 # units are microns
 
+# set parameters
 batch_size = 100
 num_basis = 200 # number of basis functions, N in pde-cl paper
-use_pde_cl = True
+use_pde_cl = True # use the partial differential equation constrained layer
 wavelength = 2.1 # um
 n_background = 1
 use_cpu = False
 epochs = 1000
 
-training_data_x_start = [-2,-2,0]
-training_data_x_end = [2,2,0.5]
-training_data_x_step = [0.1,0.1,0.5]
+# set the training region
+training_data_x_start = [-2,-2,-2]
+training_data_x_end = [2,2,2]
+training_data_x_step = [0.1,0.1,0.1]
 
-offset_2 = 0.05
-
-test_data_x_start = [-2,-2,0]
-test_data_x_end = [2,2,0.5]
+# set the test region
+offset = 0.05
+test_data_x_start = [-2,-2,-2]
+test_data_x_end = [2,2,2]
 test_data_x_step = [0.5,0.5,0.5]
 
-# this ensures that the current MacOS version is at least 12.3+
+# this ensures that if using Apple Silicon GPU, MPS is activated.
 print("Using Apple Silicon GPU? " + str(torch.backends.mps.is_available()))
-# this ensures that the current current PyTorch installation was built with MPS activated.
+# this ensures that the current current PyTorch installation was built with MPS activated if using Mac
 print("PyTorch install with Apple Silicon GPU support? " + str(torch.backends.mps.is_built()))
 
 dtype = torch.float
@@ -47,14 +47,18 @@ if use_cpu:
 print(f"Using {device} device")
 
 # Training data to compute weights w
+# Training data is a list of coordinates
 training_data = create_data(training_data_x_start, training_data_x_end, training_data_x_step, device)
 
-
 # Training data to compute pde loss
+# Training data is a list of coordinates
+# This is only used if the linear system is underdetermined
 if batch_size<num_basis:
-    training_data_2 = create_data(np.array(training_data_x_start)+offset_2, 
-                                np.array(training_data_x_end)+offset_2, training_data_x_step, device)
+    training_data_2 = create_data(np.array(training_data_x_start)+offset, 
+                                np.array(training_data_x_end)+offset, training_data_x_step, device)
 
+# Test data for validation of pde loss
+# Test data is a list of coordinates
 test_data = create_data(test_data_x_start, test_data_x_end, test_data_x_step, device)
 
 # Force num_basis = 1 if not using pde-cl
@@ -72,10 +76,9 @@ test_dataloader = DataLoader(test_data, batch_size=batch_size, shuffle=False)
 model = NeuralNetwork(num_basis).to(device)
 print(model)
 
-
 optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
 
-
+# PDE loss function
 def loss_fn(data, u_scatter, data_2): 
     return get_pde_loss(data, 
                         wavelength,
@@ -87,6 +90,7 @@ def loss_fn(data, u_scatter, data_2):
                         data_2=data_2,
                         ) 
 
+# Train the PINN
 test_loss_vec = []
 for t in range(epochs):
     print(f"Epoch {t+1}\n-------------------------------")
@@ -99,22 +103,21 @@ for t in range(epochs):
 print("Done!")
 
 # Save model
-
 torch.save(model.state_dict(), "model.pth")
 print("Saved PyTorch Model State to model.pth")
 
 # Load model
-
 model = NeuralNetwork(num_basis).to(device)
 model.load_state_dict(torch.load("model.pth"))
 
 
 # Use loaded model to make predictions
-
 model.eval()
 
-# eval_data = create_data(training_data_x_start, training_data_x_end, training_data_x_step, device)
-eval_data = create_data([-2,-2,0], [2,2,0.5], [0.1,0.1,0.5], device)
+# Visualize the PINN with list of coordinates
+offset_eval = 0.03
+eval_data = create_data(training_data_x_start+offset_eval, training_data_x_end+offset_eval, training_data_x_step, device)
+
 
 with torch.no_grad():
     k0 = get_k0(wavelength)

utils.py
@@ -5,6 +5,7 @@ import torch
 from torch import nn
 
 def create_data(min_vals, max_vals, spacings, device):
+    """Create a list of coordinates from a grid"""
     data_x = torch.arange(min_vals[0],max_vals[0],spacings[0],device=device)
     data_y = torch.arange(min_vals[1],max_vals[1],spacings[1],device=device)
     data_z = torch.arange(min_vals[2],max_vals[2],spacings[2],device=device)
@@ -28,7 +29,7 @@ class NeuralNetwork(nn.Module):
 
     def forward(self, x):
         u_scatter = self.linear_relu_stack(x)
-        u_scatter = torch.reshape(u_scatter, (-1,self.num_basis,2))
+        u_scatter = torch.reshape(u_scatter, (-1,self.num_basis,2)) # last dimension is the real and imaginary parts
         return u_scatter
 
 def evalulate_refractive_index(data,
@@ -37,8 +38,11 @@ def evalulate_refractive_index(data,
                               ):
     return torch.where(torch.sum(data**2,dim=1)<radius**2, 1.3, n_background)
 
+
 def get_k0(wavelength):
+    """get free space wavenumber"""
     return 2*np.pi/wavelength
+
 
 def create_plane_wave(data, 
                       wavelength,
@@ -48,6 +52,7 @@ def create_plane_wave(data,
                       tilt_theta=np.pi/2, # radians, angle from the xy plane, np.pi/2 is in the z-direction
                       tilt_phi=0, # radians, angle in the xy plane
                      ):
+    """create free space plane wave u_in"""
     """z is the direction of propagation"""
     """x into the page, y up, z to the right"""
     k0 = get_k0(wavelength)
@@ -66,8 +71,8 @@ def transform_linear_pde(data,
                          model,
                          device,
                         ):
-
-    hess = torch.vmap(torch.func.hessian(model, argnums=0),in_dims=(0))(data)
+    '''Get the right hand side of the PDE (del**2 + n**2*k0**2)*u_scatter = -(n**2-n_background**2)*k0**2*u_in))'''
+    hess = torch.vmap(torch.func.hessian(model, argnums=0),in_dims=(0))(data) # hessian
     refractive_index = evalulate_refractive_index(data, n_background) 
     
     du_scatter_xx = torch.squeeze(hess[:,:,:,:,0,0], dim=1)
@@ -90,7 +95,9 @@ def transform_affine_pde(wavelength,
                          model,
                          device,
                          ):
-    
+    '''Get the right and left hand side of the PDE (del**2 + n**2*k0**2)*u_scatter = -(n**2-n_background**2)*k0**2*u_in))'''
+
+    # get the right hand side of the PDE
     linear_pde, refractive_index, u_scatter_complex = transform_linear_pde(data,
                                                                            k0,
                                                                            n_background,
@@ -102,8 +109,10 @@ def transform_affine_pde(wavelength,
                              wavelength,
                              n_background,
                              device)
-    f = k0**2*(refractive_index**2 - n_background**2)*u_in
-    f=torch.unsqueeze(f,dim=1)
+    
+    # get the left hand side of the PDE
+    f = -k0**2*(refractive_index**2 - n_background**2)*u_in
+    f = torch.unsqueeze(f,dim=1)
     return linear_pde, f, u_scatter_complex, u_in
 
 def get_pde_loss(data, 
@@ -142,6 +151,7 @@ def get_pde_loss(data,
     # combine the scattered field with the incident field
     u_total = u_scatter_complex_combine+u_in
 
+    # if underdetermined, use the second dataset to get the loss
     if data_2 is not None:
         linear_pde, f, _, _ = \
         transform_affine_pde(wavelength,
@@ -166,7 +176,7 @@ def train(dataloader,
           optimizer,
           device,
           ):
-    
+    """Train the model for one epoch"""
     if dataloader_2 is not None:
         dataloader_2_iter = iter(dataloader_2)
     size = len(dataloader.dataset)