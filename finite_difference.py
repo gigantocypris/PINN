b53e6f7699b64a73410de57c5b643def62ce09e9
Vidya Ganapati <vganapati@gmail.com>
2023-07-06 17:34:18 -0700

    added pml

--- finite_difference.py
+++ finite_difference.py
@@ -4,11 +4,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Inputs
-x_start = [-20.0,-20.0]
-x_end = [20.0,20.0]
-x_step = [0.2,0.2]
+x_start = [-5.0,-5.0]
+x_end = [5.0,5.0]
+x_step = [0.05,0.05]
 wavelength = 1.0 # wavelength in free space
-pml_grid_points = 10
+pml_grid_points = 5
 n_background = 1.33
 radius = 3.0
 
@@ -41,38 +41,86 @@ plane_wave = amplitude*np.exp(-1j*(np.sum(k*data, axis=-1)))
 # Construct the source
 E = -(refractive_index**2-n_background**2)*k_0**2*plane_wave
 
-# Construct finite difference matrix A
+# Helper function for PML
 
-A = np.zeros([domain_size_x * domain_size_y, domain_size_x * domain_size_y])
+def e_i(i, domain_size_i, pml_grid_points, a_0=5):
+    if i < pml_grid_points or i > domain_size_i-pml_grid_points-1:
+        dist_to_edge = min(i,domain_size_i-i)
+    else:
+        dist_to_edge = 0
+    sigma = a_0*(dist_to_edge/pml_grid_points)**2
+    
+    e = 1-1j*sigma
+
+    d_dist_d_x = 0
+    if i < pml_grid_points:
+        d_dist_d_x = 2*i
+    elif i > domain_size_i-pml_grid_points-1:
+        d_dist_d_x = -2*(domain_size_i-i-1)
+
+    coeff = -1j*a_0/pml_grid_points**2
+    
+    return e, coeff, d_dist_d_x
+
+# Construct finite difference matrix A
+A = np.zeros([domain_size_x * domain_size_y, domain_size_x * domain_size_y], dtype=np.complex128)
 for i in range(0, domain_size_x):
     for j in range(0, domain_size_y):
         n = i * domain_size_y + j  # Current grid point index
-
         refractive_index_n = refractive_index[i, j]  # Refractive index at the current grid point
-        A[n, n] = -2 / dx**2 - 2 / dy**2 + k_0**2 * refractive_index_n**2
-        try:
-            A[n, (i - 1) * domain_size_y + j] = 1 / dx**2
-        except IndexError:
-            pass
-        try:
-            A[n, (i + 1) * domain_size_y + j] = 1 / dx**2
-        except IndexError:
-            pass
-        try:
-            A[n, i * domain_size_y + (j - 1)] = 1 / dy**2
-        except IndexError:
-            pass
-        try:
-            A[n, i * domain_size_y + (j + 1)] = 1 / dy**2
-        except IndexError:
-            pass
 
-# Modify matrix A for PML boundary conditions
-# XXX
+        if i < pml_grid_points or i > domain_size_x - pml_grid_points - 1 or j < pml_grid_points or j > domain_size_y - pml_grid_points - 1:
+            # PML region
+            e_x, coeff_x, d_dist_d_x  = e_i(i, domain_size_x, pml_grid_points)
+            e_y, coeff_y, d_dist_d_y = e_i(j, domain_size_y, pml_grid_points)
+            A[n, n] = (e_y/e_x)*(-2 / dx**2) - (e_x/e_y) * (2 / dy**2) + e_x*e_y*k_0**2 * refractive_index_n**2
+            
+            try:
+                A[n, (i - 1) * domain_size_y + j] = (e_y/e_x) * (1 / dx**2) - e_y*coeff_x*d_dist_d_x/(-e_x**2) * (1 / 2*dx)
+            except IndexError:
+                pass
+
+            try:
+                A[n, (i + 1) * domain_size_y + j] = (e_y/e_x) * (1 / dx**2) + e_y*coeff_x*d_dist_d_x/(-e_x**2) * (1 / 2*dx)
+            except IndexError:
+                pass
+
+            try:
+                A[n, i * domain_size_y + (j - 1)] = (e_x/e_y) * (1 / dy**2) - e_x*coeff_y*d_dist_d_y/(-e_y**2) * (1 / 2*dy)
+            except IndexError:
+                pass
+
+            try:
+                A[n, i * domain_size_y + (j + 1)] = (e_x/e_y) * (1 / dy**2) + e_x*coeff_y*d_dist_d_y/(-e_y**2) * (1 / 2*dy)
+            except IndexError:
+                pass
+
+        else:
+            # Non-PML region
+            A[n, n] = -2 / dx**2 - 2 / dy**2 + k_0**2 * refractive_index_n**2
+            try:
+                A[n, (i - 1) * domain_size_y + j] = 1 / dx**2
+            except IndexError:
+                pass
+            try:
+                A[n, (i + 1) * domain_size_y + j] = 1 / dx**2
+            except IndexError:
+                pass
+            try:
+                A[n, i * domain_size_y + (j - 1)] = 1 / dy**2
+            except IndexError:
+                pass
+            try:
+                A[n, i * domain_size_y + (j + 1)] = 1 / dy**2
+            except IndexError:
+                pass
+
+
 
 # Solve the system of equations
 U_s = np.linalg.solve(A, E.flatten())
 U_s = U_s.reshape(domain_size_x, domain_size_y)
+
 
 # Plotting the results
 plt.figure()
